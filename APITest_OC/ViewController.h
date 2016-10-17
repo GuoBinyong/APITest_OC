dc6cdf4b84d101810c6284f2009cec73c9345dea
郭斌勇 <guobinyong@qq.com>
2016-10-17 09:49:29 +0800

    创建APITest_OC工程文件！

---
+++ APITest_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APITest_OC
+//
+//  Created by 郭斌勇 on 2016/10/17.
+//  Copyright © 2016年 郭斌勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ APITest_OC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  APITest_OC
+//
+//  Created by 郭斌勇 on 2016/10/17.
+//  Copyright © 2016年 郭斌勇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
